522e837350a74c01f695451de1971fa90fe094b9
ltnal34 <chae1120e@gmail.com>
2023-10-13 18:19:17 +0900

    overflow

--- static/pc/js/comm.js
+++ static/pc/js/comm.js
@@ -1,51 +1,63 @@
 $(function () {
-    // // 로딩
-    var customElement = $("<div>", {
-        class: "loading",
-        text: "Loading...",
+  // // 로딩
+  var customElement = $("<div>", {
+    class: "loading",
+    text: "Loading...",
+  });
+  $.LoadingOverlay("show", {
+    image: "",
+    background: "rgba(0,0,0,0.2)",
+    custom: customElement,
+  });
+  setTimeout(function () {
+    $.LoadingOverlay("hide");
+  }, 2000);
+
+  // menu 창 없애기
+  $("#gnb").each(function () {
+    let $gnb = $("#gnb"),
+      $gnbLink = $gnb.find("a"),
+      $burger = $(".burger-input");
+
+    $gnbLink.click(function () {
+      if ($burger.is(":checked")) {
+        $burger.prop("checked", false);
+      } else {
+        $burger.prop("checked", true);
+      }
     });
-    $.LoadingOverlay("show", {
-        image: "",
-        background: "rgba(0,0,0,0.2)",
-        custom: customElement,
+  });
+
+  $(".container-movie").each(function () {
+    // movie
+    let $this = $(".container-movie"),
+      $closeMovie = $this.find(".link-close"),
+      $openMovie = $("#gnb .link-movie"),
+      embed = $(".player");
+
+    $(".wrapper-movie").empty();
+
+    $openMovie.on("click", function () {
+      $this.addClass("active");
+      $(".wrapper-movie").append(embed);
     });
-    setTimeout(function () {
-        $.LoadingOverlay("hide");
-    }, 2000);
-
-    // menu 창 없애기
-    $("#gnb").each(function () {
-        let $gnb = $("#gnb"),
-            $gnbLink = $gnb.find("a"),
-            $burger = $(".burger-input");
-
-        $gnbLink.click(function () {
-            if ($burger.is(":checked")) {
-                $burger.prop("checked", false);
-            } else {
-                $burger.prop("checked", true);
-            }
-        });
+    $closeMovie.on("click", function () {
+      $this.removeClass("active");
+      $(".wrapper-movie").empty();
     });
+  });
 
-    $(".container-movie").each(function () {
-        // movie
-        let $this = $(".container-movie"),
-            $closeMovie = $this.find(".link-close"),
-            $openMovie = $("#gnb .link-movie"),
-            embed = $(".player");
+  // circle mouse
+  const text = document.querySelector(".curser-text");
+  text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
 
-        $(".wrapper-movie").empty();
+  const element = document.querySelectorAll(".curser-text span");
+  for (let i = 0; i < element.length; i++) {
+    element[i].style.transform = "rotate(" + i * 18 + "deg)";
+  }
 
-        $openMovie.on("click", function () {
-            $this.addClass("active");
-            $(".wrapper-movie").append(embed);
-        });
-        $closeMovie.on("click", function () {
-            $this.removeClass("active");
-            $(".wrapper-movie").empty();
-        });
-    });
-
-    // youtube 자동재생
+  document.addEventListener("mousemove", function (e) {
+    text.style.left = e.pageX + "px";
+    text.style.top = e.pageY + "px";
+  });
 });
